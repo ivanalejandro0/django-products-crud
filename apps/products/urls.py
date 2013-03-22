d3fee2c219c138a879102d76b4f4925e630bf730
Ivan Alejandro <ivanalejandro0@gmail.com>
2013-03-22 14:44:32 -0300

    add products app

---
+++ apps/products/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+from django.views.generic import ListView
+from products.models import Product
+
+urlpatterns = patterns('products.views',
+    url(r'^list/$', ListView.as_view(
+        template_name="products/list.html",
+        model=Product), name='products_list'),
+)
