19073adba44dbc2d454341674e4b0382151c98a3
Ivan Alejandro <ivanalejandro0@gmail.com>
2013-03-27 12:25:13 -0300

    add new product feature

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('products.views',
     url(r'^list/$', ListView.as_view(
         template_name="products/list.html",
         model=Product), name='products_list'),
+    url(r'^add/$', 'add', name='product_add'),
 )
--- apps/products/views.py
+++ apps/products/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+from products.forms import ProductForm
+
+
+def add(request):
+    if request.POST:
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = ProductForm()
+
+    content = {'title': 'Add new product', 'form': form}
+    context_instance = RequestContext(request)
+    return render_to_response('products/form.html', content, context_instance)
